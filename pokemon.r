31774543499dee51ffa3bae69bdeecee52ea0ebd
Thierry Lacour <thi@praqma.net>
2020-09-22 16:30:55 +0200

    Clean up type-counting plot

--- pokemon.r
+++ pokemon.r
@@ -10,6 +10,17 @@ library(magrittr)
 #devtools::install_github("schochastics/Rokemon")
 library(Rokemon)
 
+## PALETTES
+
+pokemon_type_pal <- c(
+  `normal`= "#A8A77A",`fire`= "#EE8130",`water`= "#6390F0",
+  `electric`= "#F7D02C",`grass`= "#7AC74C",`ice`= "#96D9D6",
+  `fighting`= "#C22E28",`poison`= "#A33EA1",`ground`= "#E2BF65",
+  `flying`= "#A98FF3",`psychic`= "#F95587",`bug`= "#A6B91A",
+  `rock`= "#B6A136",`ghost`= "#735797",`dragon`= "#6F35FC",
+  `dark`= "#705746",`steel`= "#B7B7CE",`fairy`= "#D685AD"
+)
+
 ## SCRIPT
 
 # load data
@@ -25,5 +36,12 @@ type_count <- bind_rows(type1_count, type2_count) %>% group_by(type) %>% summari
 
 # plot it out
 type_count_plot <- ggplot(type_count) +
-  aes(x = n, y = type) +
-  geom_bar(stat = "identity")
+  aes(x = n, y = reorder(type, n), fill = type) +
+  scale_fill_manual(values = pokemon_type_pal) +
+  labs(title = "Pokémon count by type") +
+  xlab("Count") +
+  ylab("Type") +
+  geom_bar(stat = "identity") +
+  guides(fill=FALSE)
+
+type_count_plot
