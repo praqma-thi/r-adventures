7e3de973ddd9bd97291acec29a135ff2f018a587
Thierry Lacour <thi@praqma.net>
2020-09-22 16:06:32 +0200

    First plot

--- pokemon.r
+++ pokemon.r
@@ -1,5 +1,24 @@
+## DEPENDENCIES
+
+# Tidyverse
 install.packages("tidyverse")
 library(tidyverse)
+library(magrittr)
 
-pokemon_db_file <- file.choose()
-pokemon_dt <- read.csv(pokemon_db_file)
+## SCRIPT
+
+# load data
+pokemon <- file.choose() %>% read.csv() %>% as_tibble()
+
+# select typings subset
+pokemon_typings <- pokemon %>% select(name, type1, type2)
+
+# count pokemon by type
+type1_count <- pokemon_typings %>% count(type1) %>% rename(type = type1)
+type2_count <- pokemon_typings %>% filter(type2 != "") %>% count(type2) %>% rename(type = type2)
+type_count <- bind_rows(type1_count, type2_count) %>% group_by(type) %>% summarise_all(list(~ sum(.)))
+
+# plot it out
+type_count_plot <- ggplot(type_count) +
+    aes(x = n, y = type) +
+    geom_bar(stat = "identity")
